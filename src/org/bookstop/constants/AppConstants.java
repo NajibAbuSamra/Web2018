de08b3cbf5c895125b42a1af8a29024b6240573c
Unknown <najib.as1990@gmail.com>
2018-01-21 23:39:01 +0200

    ManageDB listener and adding to constants and sql statements

--- src/org/bookstop/constants/AppConstants.java
+++ src/org/bookstop/constants/AppConstants.java
@@ -1,5 +1,21 @@
 package org.bookstop.constants;
 
-public final class AppConstants {
+import java.lang.reflect.Type;
+import java.util.Collection;
 
+import com.google.gson.reflect.TypeToken;
+
+import org.bookstop.model.*;
+
+public interface AppConstants {
+	public final String BOOKS = "books";
+	public final String BOOKS_FILE = BOOKS + ".json";
+	public final String NAME = "name";
+	public final Type BOOK_COLLECTION = new TypeToken<Collection<Book>>() {}.getType();
+	//derby constants
+	public final String DB_NAME = "DB_NAME";
+	public final String DB_DATASOURCE = "DB_DATASOURCE";
+	public final String PROTOCOL = "jdbc:derby:"; 
+	public final String OPEN = "Open";
+	public final String SHUTDOWN = "Shutdown";
 }
--- src/org/bookstop/constants/SQLstatements.java
+++ src/org/bookstop/constants/SQLstatements.java
@@ -1,5 +1,21 @@
 package org.bookstop.constants;
 
+import org.bookstop.dataAccess.DataContract;
+
 public final class SQLstatements {
+
+	public final static String CREATE_BOOKS_TABLE = "CREATE TABLE " + DataContract.BooksTable.TABLE_NAME + "("
+			+ DataContract.BooksTable.COL_ID + " INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)"
+			+ DataContract.BooksTable.COL_NAME + " varchar(255) NOT NULL,"
+			+ DataContract.BooksTable.COL_IMG + " varchar(255) NOT NULL,"
+			+ DataContract.BooksTable.COL_PRICE + " DOUBLE NOT NULL,"
+			+ DataContract.BooksTable.COL_DESCRIPTION + " varchar(255) NOT NULL,"
+			+ DataContract.BooksTable.COL_LINK + "varchar(255) NOT NULL,"
+			+ "CONSTRAINT primary_key PRIMARY KEY ("+DataContract.BooksTable.COL_ID+"))";
+
+
+	public final static String INSERT_BOOK_STMT = "INSERT INTO " + DataContract.BooksTable.TABLE_NAME + " VALUES(?,?,?,?,?)";
+	public final static String SELECT_ALL_BOOKS_STMT = "SELECT * FROM " + DataContract.BooksTable.TABLE_NAME;
+	public final static String SELECT_BOOKS_BY_ID_STMT = "SELECT * FROM " + DataContract.BooksTable.TABLE_NAME + "WHERE " + DataContract.BooksTable.COL_ID + "=?";
 
 }
