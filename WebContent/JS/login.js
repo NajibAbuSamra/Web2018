715fd8636bd7250ccb600546e0792bad84a190e7
Unknown <tonytanios1994@gmail.com>
2018-02-02 00:35:11 +0200

    Login button, input is read in .js

--- WebContent/JS/login.js
+++ WebContent/JS/login.js
@@ -1,4 +1,13 @@
 /**
  * 
  */
-
+window.onload=function(){
+	var btn = document.getElementById("butt");
+	btn.addEventListener('click', myFunc);
+	
+	function myFunc() {
+		var usrname = document.getElementById("userName").value;
+		var pass = document.getElementById("password").value;
+		
+	}
+}
