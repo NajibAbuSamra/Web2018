4b4039f576ec9e2982b40e5fd3d17b1576b64de4
Unknown <tonytanios1994@gmail.com>
2018-02-02 01:17:28 +0200

    Called servlet with json in login.js

--- WebContent/JS/login.js
+++ WebContent/JS/login.js
@@ -6,8 +6,20 @@ window.onload=function(){
 	btn.addEventListener('click', myFunc);
 	
 	function myFunc() {
-		var usrname = document.getElementById("userName").value;
-		var pass = document.getElementById("password").value;
-		
+		var username = document.getElementById("userName").value;
+		var password = document.getElementById("password").value;
+		var formObject =  {
+            "uName": username,
+            "uPass": password
+        }
+		$.ajax({
+			type : "POST",
+			dataType: 'json',		
+			contentType: "application/json; charset=UTF-8",
+			url : "/Login",
+			data : JSON.stringify(jsObject)		
+		}).done(function(data) {
+				alert(data["username"]);
+		   });
 	}
 }
