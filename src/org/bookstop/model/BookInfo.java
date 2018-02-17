a3e176529decda17b8333345f23ddd5938062a3a
Unknown <najib.as1990@gmail.com>
2018-02-17 22:44:15 +0200

    Created BookInfo model
    it will hold more detail about a book

---
+++ src/org/bookstop/model/BookInfo.java
@@ -0,0 +1,16 @@
+package org.bookstop.model;
+
+import java.util.ArrayList;
+
+public class BookInfo {
+	private int bookId;
+	private String name;
+	private String img;
+	private double price;
+	private String description;
+	private String link;
+	private int likes;
+	private ArrayList<Review> reviews;
+	
+	
+}
