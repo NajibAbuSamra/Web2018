b1af8606a83923fe7b040eee4c1bf6b90778f2c3
Unknown <najib.as1990@gmail.com>
2018-02-18 17:52:16 +0200

    added browse function in nagular

--- WebContent/JS/myApp.js
+++ WebContent/JS/myApp.js
@@ -8,20 +8,22 @@ angular.module('myApp',[])
 	$scope.errorBox="";
 	
 	var loggedUser = "";
-	
-	
+	var loggedPass = ""
+	/*LOGIN*/
 	$scope.loginFunc = function(){
 		var val = JSON.stringify({uName:$scope.uName , uPass:$scope.uPass});
 		$http.post("/Web2018/Login",val).
 		success(function(data,status,headers,config){
 			$scope.loggedIn=true;
 			loggedUser = data.username;
+			loggedPass = data.password;
 			 $scope.answer = loggedUser;
 			}).
 		error(function(data,status,headers,config){
 			$scope.errorBox="Error";
 			});
 	}
+	/*REGISTER*/
 	$scope.registerFunc = function() {
 		/*VALIDITY CHECK HERE*/
 		
@@ -40,11 +42,29 @@ angular.module('myApp',[])
 						$scope.errorBox = "User already exists";
 				}).done(function(data) {});
 	};
-	
+	/*TOGGLE*/
 	$scope.toggleRegister = function(){
 		$scope.registered = !($scope.registered);
 	}
+	/*LOGOUT*/
 	$scope.logout = function(){
-		$scope.loggedIn = !($scope.loggedIn);
+		$scope.loggedIn = false;
 	}
+	/*BROWSE*/
+	$scope.browse = function(){
+		$window.alert(greeting);
+		var val = JSON.stringify({uName:loggedUser , uPass:loggedPass})
+		$http.post("/Web2018/GetAvailableBooks",val).success(
+				function(data, status, headers, config) {
+					/*PUT BOOKS IN PROPER DIV*/
+					$scopse.browseBooks
+				}).error(
+				function(data, status, headers, config) {
+					/*SHOW ERROR*/
+					if(status == 401){
+						/*wrong password, log user out and display error*/
+					}
+				}).done(function(data) {});
+	}
+	
 }]);
