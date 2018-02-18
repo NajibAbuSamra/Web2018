4cfccc042c735b7a50d90e69676a6507815e7580
Unknown <najib.as1990@gmail.com>
2018-02-18 13:36:51 +0200

    changed BookInfo class

--- src/org/bookstop/model/BookInfo.java
+++ src/org/bookstop/model/BookInfo.java
@@ -2,15 +2,32 @@ package org.bookstop.model;
 
 import java.util.ArrayList;
 
-public class BookInfo {
-	private int bookId;
-	private String name;
-	private String img;
-	private double price;
-	private String description;
-	private String link;
+public class BookInfo extends Book{
 	private int likes;
 	private ArrayList<Review> reviews;
 	
+	public BookInfo(int bookId, String name, String img, double price, String description, String link, int likes, ArrayList<Review> reviews) {
+		super(bookId, name, img, price, description, link);
+		this.setLikes(likes);
+		this.reviews = new ArrayList<Review>();
+		if(reviews != null) {
+			for(Review r : reviews) {
+				this.reviews.add(r);
+			}
+		}
+		
+	}
+
+	public int getLikes() {
+		return likes;
+	}
+
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+
+	public ArrayList<Review> getReviews() {
+		return reviews;
+	}
 	
 }
