002988de16eaf723f226b6826b824b3c4068a561
Unknown <najib.as1990@gmail.com>
2018-02-20 13:45:35 +0200

    Expanded server initialization to include adding users,reviews,and likes

--- src/org/bookstop/constants/AppConstants.java
+++ src/org/bookstop/constants/AppConstants.java
@@ -10,8 +10,17 @@ import org.bookstop.model.*;
 public interface AppConstants {
 	public final String BOOKS = "books";
 	public final String BOOKS_FILE = BOOKS + ".json";
+	public final String USERS = "users";
+	public final String USERS_FILE = USERS + ".json";
+	public final String LIKES = "likes";
+	public final String LIKES_FILE = LIKES + ".json";
+	public final String REVIEWS = "reviews";
+	public final String REVIEWS_FILE = REVIEWS + ".json";
 	public final String NAME = "name";
 	public final Type BOOK_COLLECTION = new TypeToken<Collection<Book>>() {}.getType();
+	public final Type USER_COLLECTION = new TypeToken<Collection<User>>() {}.getType();
+	public final Type REVIEW_COLLECTION = new TypeToken<Collection<Review>>() {}.getType();
+	public final Type LIKE_COLLECTION = new TypeToken<Collection<Like>>() {}.getType();
 	//derby constants
 	//TODO: might need to change DB_NAME and DB_DATASOURCE
 	public final String DB_NAME = "DB_NAME";
